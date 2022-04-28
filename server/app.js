ba7ffbc72acd31fb9b49b0fd1e2e002072a40ac6
Wesley <leewes5928@users.noreply.github.com>
2022-04-28 04:14:14 -0700

    add get endpoint for user resource

--- server/app.js
+++ server/app.js
@@ -5,8 +5,18 @@ const app = express();
 
 app.use(express.static(path.resolve(__dirname, "..", "build")));
 
+app.get("/api/users", async (req, res) => {
+    try {
+        const user = await db.select().table('users');
+        res.status(200).json({ success: true, data: user });
+    } catch (err) {
+        console.error("Error fetching user.", err);
+        res.status(400).json({ success: false });
+    };
+});
+
 app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "..", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "..", "build", "index.html"));
 });
 
 module.exports = app;
