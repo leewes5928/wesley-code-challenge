974b79efdd66e4036354a833475b98b5524433c9
Wesley <leewes5928@users.noreply.github.com>
2022-04-28 05:18:34 -0700

    add fetchUser

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import axios from 'axios';
+import { useEffect } from 'react';
 import './App.css';
 import SubmitImage from './components/SubmitImage';
 import SubmitName from './components/SubmitName';
 
 function App() {
+  useEffect(()=> {
+
+    (async function fetchUser() {
+      const { data: { data: user }} = await axios.get("/api/users");
+    })();
+    
+  },[]);
+  
   return (
     <div className="App">
       Name
-      <img alt="Avatar Image"></img>
+      <img alt="Avatar"></img>
       <SubmitImage />
       <SubmitName />
     </div>
