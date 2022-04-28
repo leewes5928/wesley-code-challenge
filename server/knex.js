e55c1cfb87a24fa622d32db3660809dcf33a0b1c
Wesley <leewes5928@users.noreply.github.com>
2022-04-28 03:38:16 -0700

    add knex settings

---
+++ server/knex.js
@@ -0,0 +1,7 @@
+const knex = require("knex");
+
+const db = knex({
+    client: "pg",
+    connection: process.env.DB_URL || `postgres://${process.env.DB_USER}:${process.env.DB_PW}@127.0.0.1:5432/${DB_NAME}`,
+    searchPath: "public",
+});
