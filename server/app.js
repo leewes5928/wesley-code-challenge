d5f27fd66dd716a033b70152475f345d12f11f1a
Wesley <leewes5928@users.noreply.github.com>
2022-04-28 03:54:50 -0700

    add scripts for serving static files

--- server/app.js
+++ server/app.js
@@ -4,3 +4,9 @@ const db = require("./knex.js");
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, "..", "build")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "..", "build", "index.html"))
+});
+
+module.exports = app;
--- server/knex.js
+++ server/knex.js
@@ -5,3 +5,5 @@ const db = knex({
     connection: process.env.DB_URL || `postgres://${process.env.DB_USER}:${process.env.DB_PW}@127.0.0.1:5432/${DB_NAME}`,
     searchPath: "public",
 });
+
+module.exports = db;
