5df6ae68f330a6ee7b09b0ffa0c46d9093d432b3
Wesley <leewes5928@users.noreply.github.com>
2022-04-28 01:02:23 -0700

    add function to handle submitting images

--- app/components/ImgSubmission/ImgSubmission.jsx
+++ app/components/ImgSubmission/ImgSubmission.jsx
@@ -1,8 +1,25 @@
-export default function ImgSubmission({ handleImg }) {
+import { createRef } from "react";
+import axios from 'axios';
+
+export default function ImgSubmission({ handleImg, endpoint }) {
+  let imgRef = createRef();
+
+  function convertImgToString() {
+  }
+
+  function handleClick() {
+    imgRef.current.click();
+  }
+
+  async function handleUpload(e) {
+    let imgString = convertImgToString(e.target.value);
+    let response = await axios.put(endpoint, {userImg: imgString});
+    handleImg(`data${response.data.img}`)
+  }
     return (
       <div>
-        <input type="file"/>
-        <button>Upload Avatar</button>
+        <input style={{display: "none"}} type="file" ref={imgRef} onChange={handleUpload}/>
+        <button onClick={handleClick}>Upload Avatar</button>
       </div>
     );
   }
