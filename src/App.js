175f65d1901dff7c4d743b3ef6906a927c88f353
Wesley <leewes5928@users.noreply.github.com>
2022-04-28 05:31:07 -0700

    refactor app to use one state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import axios from 'axios';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import SubmitImage from './components/SubmitImage';
 import SubmitName from './components/SubmitName';
 
 function App() {
   useEffect(()=> {
-
     (async function fetchUser() {
       const { data: { data: user }} = await axios.get("/api/users");
+      setUser(user[0]);
     })();
-    
   },[]);
-  
+
+  let [user, setUser] = useState({});
+
   return (
     <div className="App">
-      Name
-      <img alt="Avatar"></img>
-      <SubmitImage />
-      <SubmitName />
+      {user.name}
+      <img src={`data:image/png;base64,${user.img}`} alt="Avatar"></img>
+      <SubmitImage userSetter={setUser} />
+      <SubmitName userSetter={setUser} />
     </div>
   );
-}
+};
 
 export default App;
