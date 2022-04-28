4732c27bcc42b52980c50356ceecf0da02ec160a
Wesley <leewes5928@users.noreply.github.com>
2022-04-28 04:20:36 -0700

    add post endpoint for user resource

--- server/app.js
+++ server/app.js
@@ -15,6 +15,16 @@ app.get("/api/users", async (req, res) => {
     };
 });
 
+app.post("/api/users", async (req, res) => {
+    try {
+        const user = await db.select().table('users').insert( req.body.user );
+        res.status(200).json({ success: true, data: user });
+    } catch (err) {
+        console.err("Error posting user.", err);
+        res.status(400).json({ success: false });
+    }
+});
+
 app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "..", "build", "index.html"));
 });
