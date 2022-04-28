6fe90494955df4823cfd95d9279b7233cdceab04
Wesley <leewes5928@users.noreply.github.com>
2022-04-28 06:12:02 -0700

    add input for SubmitName to change name on keypress

--- src/App.js
+++ src/App.js
@@ -1,35 +1,37 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import './App.css';
-import SubmitImage from './components/SubmitImage';
-import SubmitName from './components/SubmitName';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./App.css";
+import SubmitImage from "./components/SubmitImage";
+import SubmitName from "./components/SubmitName";
 
 function App() {
-  useEffect(()=> {
+  useEffect(() => {
     (async function fetchUser() {
-      const { data: { data: user }} = await axios.get("/api/users");
+      const {
+        data: { data: user },
+      } = await axios.get("/api/users");
       setUser(user[0]);
     })();
-  },[]);
+  }, []);
 
   let [user, setUser] = useState({});
 
   function setImg(image) {
-    setUser({...user, img: image});
-  };
+    setUser({ ...user, img: image });
+  }
 
   function setName(name) {
-    setUser({...user, name: name});
-  };
-  
+    setUser({ ...user, name: name });
+  }
+
   return (
     <div className="App">
-      {user.name}
       <img src={`data:image/png;base64,${user.img}`} alt="Avatar"></img>
+      <div className="name">{user.name}</div>
       <SubmitImage uid={user.user_id} imageSetter={setImg} />
       <SubmitName uid={user.user_id} nameSetter={setName} />
     </div>
   );
-};
+}
 
 export default App;
--- src/components/SubmitName.js
+++ src/components/SubmitName.js
@@ -1,7 +1,19 @@
-export default function SubmitName() {
-    return (
+import axios from "axios";
+
+export default function SubmitName({ uid, nameSetter }) {
+  async function handleNameChange(e) {
+    if (e.keyCode === 13) {
+      let response = await axios.patch(`/api/users/${uid}`, {
+        name: e.target.value,
+      });
+      nameSetter(response.data.data[0].name);
+      e.target.value = "";
+    }
+  }
+
+  return (
     <div>
-        SubmitName
+      <input type="text" onKeyDown={handleNameChange}></input>
     </div>
-    )
+  );
 }
